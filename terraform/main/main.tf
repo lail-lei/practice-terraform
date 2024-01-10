7db5e5fdd4c4ae7c15422985ce6e0e2dff58598a
lailei-ikea <lailei.forouraghi@ingka.ikea.com>
2024-01-10 18:14:25 -0500

    fix wif provider and comments

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -31,6 +31,7 @@ resource "google_app_engine_application" "required_app_engine" {
   database_type = "CLOUD_DATASTORE_COMPATIBILITY"
 }
 
+# Create Cloud Run API + service account and artifactory registry (used in API repo's deployment workflows)
 module "fso_api" {
   source = "./modules/fso_api"
   service_name                           = "fso-api"
@@ -41,13 +42,10 @@ module "fso_api" {
   cloud_run_min_scale                    = var.api_cloud_run_min_scale
 }
 
-module "bigquery" {
-  source = "./modules/bigquery"
-  app_dataset_id = var.bq_app_dataset_id
-  location = var.bq_location
-}
-
-# Create cloud function
+# Create firestore db export cloud function (for both backups and writing application data to bq for analytics)
+# Creates storage bucket to store cloud function source code
+# Creates storage bucket to store db exports (backups) 
+# Creates cloud scheduler and pub/sub topic to trigger cloud function
 module "firestore_export" {
   source = "./modules/firestore_export"
   project               = var.project
@@ -56,4 +54,11 @@ module "firestore_export" {
   export_retention_days = var.db_export_retention_days
   db_export_bucket      = var.db_export_bucket
   cf_source_bucket      = var.cf_source_bucket
+}
+
+# Creates BigQuery application data dataset 
+module "bigquery" {
+  source = "./modules/bigquery"
+  app_dataset_id = var.bq_app_dataset_id
+  location = var.bq_location
 }
